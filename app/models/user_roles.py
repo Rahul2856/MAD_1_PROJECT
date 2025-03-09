d4ec39a268bcdee28afccb0444bf2c86a1c86ef2
Rahul2856 <23f1001252@ds.study.iitm.ac.in>
2025-03-10 00:54:14 +0530

    added role and user_roles models and updated authentication flow

---
+++ app/models/user_roles.py
@@ -0,0 +1,7 @@
+
+from app.models import db
+
+class UserRoles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
