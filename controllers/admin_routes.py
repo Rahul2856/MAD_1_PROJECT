cee1a1444809f6e2e90526c90c8363c5e384796d
Rahul2856 <23f1001252@ds.study.iitm.ac.in>
2025-03-26 00:06:34 +0530

    successfully implemented the features add/edit/delete for subjects

--- controllers/admin_routes.py
+++ controllers/admin_routes.py
@@ -1,12 +1,14 @@
 from app import app
-from flask import render_template,request,redirect, url_for
+from flask import render_template,request,redirect, url_for, flash
 from controllers.rbac import adminlogin_required
-from models import Subject ,db
+from models import Subject ,db 
 
 @adminlogin_required
 @app.route("/admin_dashboard")
 def admin_dashboard():
-    return render_template("admin_templates/admin_dashboard.html")
+    subjects = Subject.query.all()  # This will return a list of all subjects
+
+    return render_template("admin_templates/admin_dashboard.html", subjects=subjects)
 
 
 @adminlogin_required
@@ -25,3 +27,48 @@ def add_subject():
             return redirect(url_for("add_subject"))
 
     return render_template("admin_templates/add_subject.html")
+
+@adminlogin_required
+@app.route("/edit_subject/<int:subject_id>", methods=['GET','POST'])
+def edit_subject(subject_id):
+    subject = Subject.query.get_or_404(subject_id)
+
+    # If the form is submitted (POST method)
+    if request.method == 'POST':
+        # Get data from the form
+        name = request.form['name']
+      
+
+        # Update the subject details
+        subject.name = name
+        db.session.commit()
+
+        # Flash success message
+        flash('Subject updated successfully!', 'success')
+
+        # Redirect to the admin dashboard
+        return redirect(url_for('admin_dashboard'))
+
+    return render_template('admin_templates/edit_subject.html',subject=subject)
+
+@adminlogin_required
+@app.route("/delete_subject/<int:subject_id>", methods=['POST'])
+def delete_subject(subject_id):
+    if request.method == 'POST':
+        subject = Subject.query.get_or_404(subject_id)
+        
+        # Delete the subject
+        db.session.delete(subject)
+        db.session.commit()
+        
+        flash('Subject deleted successfully!', 'success')
+        return redirect(url_for('admin_dashboard'))
+    
+    # This will only be reached if someone somehow sends a non-POST request
+    flash('Invalid request method', 'error')
+    return redirect(url_for('admin_dashboard'))
+
+
+
+
+
