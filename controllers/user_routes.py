a7a09accb8ee4bac61c2a13d97c634172dc8276f
Rahul2856 <23f1001252@ds.study.iitm.ac.in>
2025-03-27 16:31:45 +0530

    Fixed the error to redirect to the user dashboard page from user login

--- controllers/user_routes.py
+++ controllers/user_routes.py
@@ -1,9 +1,9 @@
 from flask import render_template
-from flask_login import login_required
+from controllers.rbac import userlogin_required
 from app import app
 
 
 @app.route("/user_dashboard")
-@login_required
+@userlogin_required
 def user_dashboard():
     return render_template("user_templates/user_dashboard.html")
